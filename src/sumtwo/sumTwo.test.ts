639babf39b3ec3320417cecbe5d782b2444089f8
haiphan <hai.phan@zoho.com>
2019-06-25 00:33:36 +0300

    Add tslint

--- src/sumtwo/sumTwo.test.ts
+++ src/sumtwo/sumTwo.test.ts
@@ -1,10 +1,10 @@
-import {sumTwo, sumTwoUnsorted} from './sumTwo';
+import { sumTwo, sumTwoUnsorted } from './sumTwo';
 
 test('Should work', () => {
-  const real = sumTwo([1,2,3,4,5], 4);
+  const real = sumTwo([1, 2, 3, 4, 5], 4);
   expect(real).toBeTruthy();
-  const real2 = sumTwo([1,2,3,4,5], 10);
+  const real2 = sumTwo([1, 2, 3, 4, 5], 10);
   expect(real2).toBeFalsy();
-  const real3 = sumTwoUnsorted([4,1,2,5], 6);
+  const real3 = sumTwoUnsorted([4, 1, 2, 5], 6);
   expect(real3).toBeTruthy();
 });
--- src/sumtwo/sumTwo.ts
+++ src/sumtwo/sumTwo.ts
@@ -3,34 +3,34 @@
 // if sum smaller, inc start point
 // if sum bigger, dec end point
 export const sumTwo = (arr, value) => {
-	let start = 0;
-	let end = arr.length - 1;
-	let sum;
-	while (start < end) {
-		sum = arr[start] + arr[end];
-		if (sum === value) {
-			return true;
-		}
-		end -= sum > value ? 1 : 0;
-		start += sum < value ? 1 : 0;
-	}
-	return false;
+  let start = 0;
+  let end = arr.length - 1;
+  let sum;
+  while (start < end) {
+    sum = arr[start] + arr[end];
+    if (sum === value) {
+      return true;
+    }
+    end -= sum > value ? 1 : 0;
+    start += sum < value ? 1 : 0;
+  }
+  return false;
 };
 
 // if arrary not sorted
 
 export const sumTwoUnsorted = (arr, value) => {
-	let searchValues = new Set();
-	let res = false;
-	arr.forEach((num) => {
-		if (res) {
-			return;
-		}
-		if (searchValues.has(num)) {
-			res = true;
-			return;
-		}
-		searchValues.add(value - num);
-	});
-	return res;
+  const searchValues = new Set();
+  let res = false;
+  arr.forEach((num) => {
+    if (res) {
+      return;
+    }
+    if (searchValues.has(num)) {
+      res = true;
+      return;
+    }
+    searchValues.add(value - num);
+  });
+  return res;
 };
