bb97fffe80bae5704dbf17d17dce0a0a93bdbf0c
haiphan <hai.phan@zoho.com>
2019-06-25 19:15:16 +0300

    Custom sort string fixxx

--- src/sortString/sortString.test.ts
+++ src/sortString/sortString.test.ts
@@ -1,7 +1,11 @@
-import { sortStringNormal } from './sortString';
+import { sortStringFast, sortStringNormal } from './sortString';
 
 test('Queue should work', () => {
   const str = 'abcdfegb';
   const pat = 'bkdpsc';
   expect(sortStringNormal(str, pat)).toEqual('bbdcafeg');
+  expect(sortStringFast(str, pat)).toEqual('bbdcafeg');
+  const str2 = 'abcd';
+  const pat2 = 'cba';
+  expect(sortStringFast(str2, pat2)).toEqual('cbad');
 });
--- src/sortString/sortString.ts
+++ src/sortString/sortString.ts
@@ -25,3 +25,26 @@ export const sortStringNormal = (sToSort, pattern): string => {
   arr.sort((a, b) => customSort(a, b, pattern));
   return arr.join('');
 };
+
+// O(N) time, using hash map
+export const sortStringFast = (sToSort, pattern): string => {
+  const arr = sToSort.split('');
+  // number of occurrence of each char
+  const charCount = arr.reduce((acc, c) => {
+    return {
+      ...acc,
+      [c]: (acc[c] || 0) + 1
+    };
+  }, {});
+  // Go through the pattern and add those letters with exact frequencies
+  const  firstPart = pattern.split('').reduce((acc, c) => {
+    const pad = c.repeat(charCount[c] || 0);
+    delete charCount[c];
+    return `${acc}${pad}`;
+  }, '');
+  const secondPart = Object.keys(charCount).reduce((acc, c) => {
+    const pad = c.repeat(charCount[c]);
+    return `${acc}${pad}`;
+  }, '');
+  return `${firstPart}${secondPart}`;
+};
